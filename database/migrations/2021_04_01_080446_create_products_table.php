e17794546ee59b0d94ec091f44a2ea729bb6b6cb
Faisal Hussain <faisal@base29.com>
2021-04-01 13:11:53 +0500

    created controller for product

--- database/migrations/2021_04_01_080446_create_products_table.php
+++ database/migrations/2021_04_01_080446_create_products_table.php
@@ -15,6 +15,10 @@ class CreateProductsTable extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->decimal('price', 5, 2);
+            $table->string('description');
+            $table->string('slug');
             $table->timestamps();
         });
     }
